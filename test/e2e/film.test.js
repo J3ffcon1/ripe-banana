fbf9fe7cf9c45391eb106b603ee4eda9700a4880
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-22 12:11:19 -0700

    started working on film tests and routes

---
+++ test/e2e/film.test.js
@@ -0,0 +1,83 @@
+const { assert } = require('chai');
+const Film = require('../models/film');
+const Studio = require('../models/studio');
+const Actor = require('../models/actor');
+const request = require('./request');
+const { dropCollection } = require('./db');
+const { Types } = require('mongoose');
+
+describe('Film E2E Test', () => {
+
+    before(() => dropCollection('films'));
+    before(() => dropCollection('studios'));
+    before(() => dropCollection('actors'));
+
+    let studio1 = { name: 'Paramount Pictures' };
+
+    before(() => {
+        return request.post('/studios')
+            .send(studio1)
+            .then(({ body }) => {
+                studio1 = body;
+            });
+    });
+
+    let actor1 = { name: 'John Krasinski' };
+
+    before(() => {
+        return request.send('/actors')
+            .send(actor1)
+            .then(({ body }) => {
+                actor1 = body;
+            });
+    });
+    
+    const checkOk = res => {
+        if(!res.ok) throw res.error;
+        return res;
+    };
+
+    let film1 = {
+        title: 'A Quiet Place',
+        studio: '',
+        released: 2018,
+        cast: [{
+            characterPart: 'Lead Role',
+            actor: ''
+        }]
+    };
+
+    // it('saves a film', () => {
+    //     film1.studio = studio1._id;
+    //     film1.cast.actor = actor1._id;
+
+    //     return request.post('/films')
+    //         .send(film1)
+    //         .then(checkOk)
+    //         .then(({ body }) => {
+    //             const { _id, __v } = body;
+    //             assert.ok(_id);
+    //             assert.equal(__v, 0);
+    //             assert.deepEqual(body, {
+    //                 ...film1,
+    //                 _id, __v
+    //             });
+    //             film1 = body;
+    //         });
+    // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ test/e2e/review.test.js
@@ -0,0 +1,27 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+
+describe('Review E2E Test', () => {
+
+    before(() => dropCollection('reviews'));
+    before(() => dropCollection('films'));
+    before(() => dropCollection('reviewer'));
+
+    let film1 = {
+        name: 'A Quiet Place'
+    };
+
+    before(() => {
+        return request.post('/films')
+            .send(film1)
+            .then(({ body }) => {
+                film1 = body;
+            });
+    });
+
+    let review1 = {
+        rating: 5,
+        reviewer
+    }
+});
