ceb433139c181882f671b5724c14599e70758e92
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-23 16:58:11 -0700

    created router for review.js

---
+++ test/e2e/review.test.js
@@ -0,0 +1,69 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+
+describe('Review E2E Test', () => {
+
+    before(() => dropCollection('reviews'));
+    before(() => dropCollection('reviewers'));
+    before(() => dropCollection('films'));
+
+    let traverse = {
+        name: 'Peter Traverse',  //make a mock critic to test
+        company: 'Rolling Stones'
+    };
+
+    before(() => {
+        return request.post('/reviewers')
+            .send(traverse)
+            .then(({ body }) => {
+                traverse = body;
+            });
+    });
+
+    let scottpilgrim = {
+        title: 'Scott Pilgrim vs. The World', //make a  mock film to test
+        studio: 'Universal Studios',
+        released: 2010,
+        cast: []
+    };
+
+    before(() => {
+        return request.post('/films')  
+            .send(scottpilgrim)
+            .then(({ body }) => {
+                scottpilgrim = body;
+            });
+    });
+
+    let review1 = {
+        rating: 3.5,
+        review :'Rolling Stone Magazine'
+    };
+
+    const checkOK = res => {
+        if(!res.ok) throw res.error;
+        return res;
+    };
+
+    it('saves a review', () => {
+        review1.reviewer = traverse._id;
+        review1.film = scottpilgrim._id;
+        return request.post('/reviews')
+            .send(review1)
+            .then(checkOK)
+            .then(({ body }) => {
+                const { _id, __v, createdAt, updatedAt } = body;
+                assert.ok(_id);
+                assert.ok(createdAt);
+                assert.ok(updatedAt);
+                assert.deepEqual(body, {
+                    ...review1,
+                    _id, __v, createdAt, updatedAt
+                });
+                review1 = body;
+            });
+    });
+
+
+});
