c3fe2a038a2a21d95f1c72066e639b2837feb97f
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-24 10:01:53 -0700

    actor test fucking done

--- test/e2e/actor.test.js
+++ test/e2e/actor.test.js
@@ -4,25 +4,21 @@ const { dropCollection } = require('./db');
 const { Types } = require('mongoose');
 
 describe.only('Actor E2E Test', () => {
-
+    before(() => dropCollection('studios'));
     before(() => dropCollection('actors'));
     before(() => dropCollection('films'));
-
-    let film1 = {
-        title: 'Scott Pilgrim Vs. the World',
-        studio: Types.ObjectId,
-        released: 2010,
-        cast: []
-    };
-
-    // before(() => {
-    //     return request.post('/films')
-    //         .send(film1)
-    //         .then(({ body }) => {
-    //             film1 = body;
-    //         });
-    // });
     
+    let studio1 = ({ name: 'Universal Pictures' });
+
+
+    before(() => {
+        return request.post('/studios')
+            .send(studio1)
+            .then(({ body }) => {
+                studio1 = body;
+            });
+    });
+
     let actor1 = {
         name: 'John Krasinski',
         dob: new Date(1979, 9, 20),
@@ -39,7 +35,7 @@ describe.only('Actor E2E Test', () => {
         if(!res.ok) throw res.error;
         return res;
     };
-
+    
     it('posts an actor to db', () => {
         return request.post('/actors')
             .send(actor1)
@@ -54,66 +50,73 @@ describe.only('Actor E2E Test', () => {
                     _id, __v, dob
                 });
                 actor1 = body;
-                film1.cast[0].actor = actor1._id;
             });
     });
     
     
     
     it('gets an actor by id', () => {
+        let film1 = {
+            title: 'Scott Pilgrim Vs. the World',
+            studio: studio1._id,
+            released: 2010,
+            cast: [{
+                role: 'supporting role',
+                actor: actor1._id
+            }]
+        };
         return request.post('/films')
             .send(film1)
             .then(({ body }) => {
                 film1 = body;
-                return request.post('/actors');
-            })
-            .send(actor2)
-            .then(checkOk)
-            .then(({ body }) => {
-                actor2 = body;
-                return request.get(`/actors/${actor2._id}`);
+                return request.get(`/actors/${actor1._id}`);
             })
             .then(({ body }) => {
                 assert.deepEqual(body, {
-                    ...actor2,
+                    ...actor1,
                     films: [{
                         _id: film1._id,
                         title: film1.title,
-                        released: film1.released
+                        released: film1.released,
                     }]
                 });
             });
     });
-
+    
     const getFields = ({ _id, name }) => {
         return {
             _id, name
         };
     };
-
+    
     it('gets all actors', () => {
-        return request.get('/actors')
+        return request.post('/actors')
+            .send(actor2)
             .then(checkOk)
             .then(({ body }) => {
-                assert.deepEqual(body, [actor1, actor2].map(getFields));
+                actor2 = body;
+                return request.get('/actors')
+                    .then(checkOk)
+                    .then(({ body }) => {
+                        assert.deepEqual(body, [actor1, actor2].map(getFields));
+                    });
             });
     });
 
     it('updates an actor', () => {
-        actor2.name = 'Aubrey Christina Plaza';
+        actor1.name = 'John Burke Krasinski';
         
-        return request.put(`/actors/${actor2._id}`)
-            .send(actor2)
+        return request.put(`/actors/${actor1._id}`)
+            .send(actor1)
             .then(checkOk)
             .then(({ body }) => {
-                assert.deepEqual(body, actor2);
-                return request.get(`/actors/${actor2._id}`);
+                assert.deepEqual(body, actor1);
+                return request.get(`/actors/${actor1._id}`);
             })
             .then(({ body }) => {
-                assert.deepEqual(body, actor2);
+                assert.deepEqual(body.name, actor1.name);
             });
     });
-
     it('removes an actor', () => {
         return request.delete(`/actors/${actor1._id}`)
             .then(() => {
