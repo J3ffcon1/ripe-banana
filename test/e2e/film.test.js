ebd319185353bf2b30cb38e6a50b3bd4a4501e5d
Tasha R Zuniga <Tashazun@gmail.com>
2018-04-25 12:55:47 -0700

    fucking movies

--- test/e2e/film.test.js
+++ test/e2e/film.test.js
@@ -1,93 +1,132 @@
-// const { assert } = require('chai');
-// const request = require('./request');
-// const { dropCollection } = require('./db');
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
 
-// describe('Film E2E Test', () => {
+describe.only('Film E2E Test', () => {
 
-//     before(() => dropCollection('films'));
-//     before(() => dropCollection('studios'));
-//     before(() => dropCollection('actors'));
-
-//     let studio1 = { name: 'Paramount Pictures' };
-
-//     before(() => {
-//         return request.post('/studios')
-//             .send(studio1)
-//             .then(({ body }) => {
-//                 studio1 = body;
-//             });
-//     });
-
-//     let actor1 = { name: 'John Krasinski' };
-
-//     before(() => {
-//         return request.post('/actors')
-//             .send(actor1)
-//             .then(({ body }) => {
-//                 actor1 = body;
-//             });
-//     });
+    before(() => dropCollection('films'));
+    before(() => dropCollection('studios'));
+    before(() => dropCollection('actors'));
+    before(() => dropCollection('reviewers'));
+    before(() => dropCollection('reviews'));
     
-//     const checkOk = res => {
-//         if(!res.ok) throw res.error;
-//         return res;
-//     };
+    let studio1 = { name: 'Paramount Pictures' };
 
-//     let film1 = {
-//         title: 'A Quiet Place',
-//         studio: '',
-//         released: 2018,
-//         cast: []
-//     };
+    before(() => {
+        return request.post('/studios')
+            .send(studio1)
+            .then(({ body }) => {
+                studio1 = body;
+            });
+    });
 
-//     let film2 = {
-//         title: 'Scott Pilgrim Vs. the World',
-//         studio: '',
-//         released: 2010,
-//         cast: []
-//     };
+    let actor1 = { name: 'John Krasinski' };
 
-//     it('saves a film', () => {
-//         film1.studio = studio1._id;
+    before(() => {
+        return request.post('/actors')
+            .send(actor1)
+            .then(({ body }) => {
+                actor1 = body;
+            });
+    });
 
-//         return request.post('/films')
-//             .send(film1)
-//             .then(checkOk)
-//             .then(({ body }) => {
-//                 const { _id, __v } = body;
-//                 assert.ok(_id);
-//                 assert.equal(__v, 0);
-//                 assert.deepEqual(body, {
-//                     ...film1,
-//                     _id, __v,
-//                 });
-//                 film1 = body;
-//             });       
-//     });
+    let reviewer1 = { 
+        name: 'Some Gal',
+        company: 'Some Company' };
 
-//     it('gets a film by id', () => {
-//         return request.post('/films')
-//             .send(film2)
-//             .then(checkOk)
-//             .then(({ body }) => {
-//                 film2 = body;
-//                 return request.get(`/films/${film1._id}`);
-//             })
-//             .then(({ body }) => {
-//                 assert.deepEqual(body, film1);
-//             });
-//     });
+    before(() => {
+        return request.post('/reviewers')
+            .send(reviewer1)
+            .then(({ body }) => {
+                reviewer1 = body;
+            });
+    });
 
+    const checkOk = res => {
+        if(!res.ok) throw res.error;
+        return res;
+    };
 
+    let film1 = {
+        title: 'A Quiet Place',
+        studio: '',
+        released: 2018,
+        cast: [{
+            role: 'grizzled farm dad',
+            actor: ''
+        }]
+    };
 
+    let film2 = {
+        title: 'fuck this shiiiiiiiiiiiiiiiit',
+        studio: '',
+        released: 2018,
+        cast: [{
+            role: 'going to blow my brains out',
+            actor: ''
+        }]
+    };
 
+    before(() => {
+        film2.studio = studio1._id;
+        film2.cast[0].actor = actor1._id;
+        return request.post('/films')
+            .send(film2)
+            .then(({ body }) => {
+                film2 = body;
+            });
+    });
 
+    it('saves a film', () => {
+        film1.studio = studio1._id;
+        film1.cast[0].actor = actor1._id;
 
+        return request.post('/films')
+            .send(film1)
+            .then(checkOk)
+            .then(({ body }) => {
+                const { _id, __v } = body;
+                assert.ok(_id);
+                assert.equal(__v, 0);
+                assert.deepEqual(body.cast[0].actor, film1.cast[0].actor);       
+            });
+    });
 
+    const comeBack = doc => JSON.parse(JSON.stringify(doc.toJSON()));
 
+    it('get film by id', () => {  
+        
+        let review1 = {
+            rating: 5,
+            reviewer: reviewer1._id,
+            review: 'It was ok',
+            film: film2._id,
+            createdAt: new Date(),
+            UpdatedAt: new Date()
+        };
+        
+        return request.post('/reviews')
+            .send(review1)
+            .then(checkOk)
+            .then(({ body }) => {
+                review1 = body;
+                return request.get(`/films/${film2._id}`).then(comeBack);
+            })
+            .then(({ body }) => {
+                console.log('error', body);
+                assert.equal(body, {
+                    ...film2,
+                    reviews: [{
+                        _id: review1._id,
+                        rating: review1._rating,
+                        review: review1.review,
+                        reviewer: {
+                            _id: reviewer1._id,
+                            name: reviewer1.name
+                        }
+                    }]
+                });
+            });
+    });
 
-
-
-
-
-// });
+});
